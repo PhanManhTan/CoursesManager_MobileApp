5f141d826ec459f2820a4a50ad693c95364755fb
HMQui <huynhqui6425@gmail.com>
2026-04-21 21:23:57 +0700

    connect db and create repositories

---
+++ app/src/main/java/com/example/myapplication/utils/Constants.java
@@ -0,0 +1,7 @@
+package com.example.myapplication.utils;
+
+public class Constants {
+    // Supabase API KEY
+    public static final String SUPABASE_URL = "https://wnwjfhdcpcvwoenyepzo.supabase.co/rest/v1/";
+    public static final String SUPABASE_API_KEY = "sb_publishable_jUUJ_rKfMlkX28hCQvA8XA_pHMLxJbG";
+}
